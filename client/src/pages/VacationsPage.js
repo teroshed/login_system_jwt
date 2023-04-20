39ea716dd8038fdbd29b794cfae7ef4da35c8411
teroshed <teroshed@gmail.com>
2023-04-20 13:07:05 +0300

    Working on add vacation form validation

--- client/src/pages/VacationsPage.js
+++ client/src/pages/VacationsPage.js
@@ -5,6 +5,9 @@ import "../styles/Vacations.css";
 import Modal from 'react-bootstrap/Modal';
 import axios from 'axios';
 
+
+var first = true;
+
 function VacationsPage() {
 
     const url = "http://localhost:3001";
@@ -13,7 +16,7 @@ function VacationsPage() {
         {
             title: "Rome",
             description: "Lorem ipsum dolor sit amet, consectetur adip",
-            startDate: new Date(2023, 5, 15),
+            startDate: new Date(2023, 4, 15),
             endDate: new Date(2023, 6, 15),
             price: 100,
             image: "rome.jpg"
@@ -21,7 +24,7 @@ function VacationsPage() {
         {
             title: "Italy",
             description: "Lorem ipsum dolor sit amet, consectetur adip",
-            startDate: new Date(2023, 5, 15),
+            startDate: new Date(2023, 2, 15),
             endDate: new Date(2023, 6, 15),
             price: 200,
             image: "italy.jpeg"
@@ -31,7 +34,7 @@ function VacationsPage() {
             description: "Lorem ipsum dolor sit amet, consectetur adip",
             startDate: new Date(2023, 5, 15),
             endDate: new Date(2023, 6, 15),
-            price: 200,
+            price: 300,
             image: "italy.jpeg"
         },
         {
@@ -39,7 +42,7 @@ function VacationsPage() {
             description: "Lorem ipsum dolor sit amet, consectetur adip",
             startDate: new Date(2023, 5, 15),
             endDate: new Date(2023, 6, 15),
-            price: 200,
+            price: 50,
             image: "italy.jpeg"
         },
         {
@@ -95,18 +98,24 @@ function VacationsPage() {
             description: "Lorem ipsum dolor sit amet, consectetur adip",
             startDate: new Date(2023, 5, 15),
             endDate: new Date(2023, 6, 15),
-            price: 200,
+            price: 30,
             image: "italy.jpeg"
         }
     ])
 
+    const [warnings, setWarnings] = useState({});
+
     const [modal, setModal] = useState(false);
 
-    var first = true;
     useEffect(() => { 
         if(first)
         {
-            
+            let vacs = structuredClone(vacations);
+            vacs.sort((a, b) => {
+                return a.startDate.getTime() - b.startDate.getTime();
+            })
+            setVacations(vacs);
+            first = false;
         }
     }, [])
 
@@ -122,19 +131,81 @@ function VacationsPage() {
     async function submitVacationForm(e)
     {
         e.preventDefault();
-        // console.log(e.target[4].files[0]);w
-        console.log("Submit: ", e);
-        let json = {name: e.target[0].value, description: e.target[1].value, startDate: e.target[2].value, endDate: e.target[3].value, price: e.target[4].value,  image: e.target[5].files[0]};
-        console.log("JSOn: ", json);
-        let a = await axios.post(url + "/addVacation", json, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        });
-        console.log(a)
+
+        let fields = {};
+        fields.name = e.target[0];
+        fields.description = e.target[1];
+        fields.startDate = e.target[2];
+        fields.endDate = e.target[3];
+        fields.price = e.target[4];
+        fields.image = e.target[5];
+
+        for(const field of e.target)
+        {
+            field.classList.remove("red-input");
+            setWarnings({});
+        }
+        if(validateForm(fields))
+        {
+            console.log(fields)
+            console.log("Submit: ", e);
+            let json = {
+                    name: fields.name.value,
+                    description: e.target[1].value,
+                    startDate: e.target[2].value,
+                    endDate: e.target[3].value,
+                    price: e.target[4].value,
+                    image: e.target[5].files[0]
+                };
+            console.log("JSOn: ", json);
+        }
+        
+        // let a = await axios.post(url + "/addVacation", json, {
+        //     headers: {
+        //         'Content-Type': 'multipart/form-data'
+        //     }
+        // });
+        // console.log(a)
         
     }
+
+    function validateForm(fields)
+    {
+        console.log("Warnings: ", warnings);
+        console.log("Validate form:", fields);
+        //Name
+        if(fields.name.value.replaceAll(" ", "") == "")
+        {
+            setWarnings({...warnings, nameWarning: "Name can't be empty"});
+            fields.name.classList.add("red-input");
+            return false;
+        }
+        if(fields.name.value.length < 15)
+        {
+            setWarnings({...warnings, nameWarning: "Name must be between 3 and 15 characters"});
+            fields.name.classList.add("red-input");
+            return false;
+
+        }
+    }
     
+    function sortChange(e)
+    {
+        let sortValue = e.target.value;
+        console.log("AA: '" +sortValue + "'")
+
+        let vacs = structuredClone(vacations);
+        vacs.sort((a, b) => {
+            if(sortValue == 1)
+                return a.startDate.getTime() - b.startDate.getTime();
+            if(sortValue == 2)
+                return a.name - b.name;
+            if(sortValue == 3)
+                return a.price - b.price;
+        });
+        setVacations(vacs);
+    }
+
   return (
     <>
 
@@ -144,7 +215,7 @@ function VacationsPage() {
                 123
             </div>
         </div> */}
-        <Modal  show={modal} onHide={() => setModal(false)}>
+        <Modal id="modal" show={modal} onHide={() => setModal(false)}>
             <div className='container'>
                 <Modal.Header bsPrefix='modalHeader' closeButton>
                     <Modal.Title>Add vacation</Modal.Title>
@@ -154,31 +225,42 @@ function VacationsPage() {
                     <form onSubmit={submitVacationForm}>
                         <div className='form-group my-2'>
                             <label htmlFor="vacName"> Vacation name</label>
-                            <input type="text" id="vacName" placeholder='Vacation name' className='form-control' required/> 
+                            <input type="text" id="vacName" placeholder='Vacation name' className='form-control ' /> 
+                            {warnings.nameWarning && <label id="vacNameWarning" htmlFor="vacName" className='text-danger text-bold '>{warnings.nameWarning}</label>}
                         </div>
                         <div className='form-group my-2'>
                             <label htmlFor="vacDescription"> Description</label>
-                            <input type="text" id="vacDescription" placeholder='Description' className='form-control'/> 
+                            <input type="text" id="vacDescription" placeholder='Description' className='form-control'/>
+                            <label id="vacDescriptionWarning" htmlFor="vacName" className='text-danger text-bold '>Description is required</label>
+
                         </div>
                         <div className='form-group my-2'>
                             <label htmlFor="vacStartDate"> Start date</label>
                             <input type="date" id="vacStartDate"  className='form-control '/> 
+                            <label id="vacStartDateWarning" htmlFor="vacName" className='text-danger text-bold '>Start date is required</label>
+
                         </div>
                         <div className='form-group my-2'>
                             <label htmlFor="vacEndDate"> End date</label>
                             <input type="date" id="vacEndDate" placeholder='Vacation name' className='form-control '/> 
+                            <label id="vacStartDateWarning" htmlFor="vacName" className='text-danger text-bold '>End date is required</label>
+
                         </div>
                         <div className='form-group my-2'>
                             <label htmlFor="vacPrice"> Price</label>
                             <input type="text" id="vacPrice" placeholder='Price' className='form-control'/> 
+                            <label id="vacStartDateWarning" htmlFor="vacName" className='text-danger text-bold '>Price is required </label>
+
                         </div>
                         <div className='form-group my-2'>
                             <label htmlFor="vacImage"> Vacation image</label>
                             <input type="file" accept="image/*" id="vacImage" placeholder='Vacation name' className='form-control '/> 
+                            <label id="vacPriceWarning" htmlFor="vacName" className='text-danger text-bold '> An image is required </label>
+
                         </div>
                         <div className="modal-buttons">
                             <button type="submit" className='btn btn-primary'> Add</button>
-                            <button type="button" className='btn btn-secondary'> Cancel</button>
+                            <button onClick={() => {setModal(false)}} type="button" className='btn btn-secondary'> Cancel</button>
                             
                         </div>
                     </form>
@@ -191,16 +273,31 @@ function VacationsPage() {
         </Modal>
         
         <div className='vacbody'>   
-        <div className='vacheader'>
-            <h3> Vacations </h3>
+            <div className='vacheader'>
+                <h3> Vacations </h3>
 
-        </div>
-        <div className='vacation-bar'>
-            <div className='right'>
-                <button type='button' className='navbutton' onClick={addVacationButton} data-toggle="modal" data-target="#examplemodal"> Add vacation </button>
             </div>
-        </div>
-            <div className='vacations-cont mx-auto p-2 '>
+            <div className='vacation-bar'>
+                <div className='right'>
+                    <button type='button' className='navbutton' onClick={addVacationButton} data-toggle="modal" data-target="#examplemodal"> Add vacation </button>
+                </div>
+            </div>
+            
+            
+            <div className='vacations-cont mx-auto p-2 container '>
+                <div className='col-4 '>
+                    <div className='d-flex ms-2'>
+                        <label htmlFor="sortSelect justify-content-start"> Sort by:</label>
+                    </div>
+                    <div className='row col-5'>
+                        <select onChange={sortChange} id="sortSelect" className='form-select mx-3 ' placeholder='select'>
+                            <option value={1}> Start date </option>
+                            <option value={2}> Name </option>
+                            <option value={3}> Price</option>
+                        </select>
+                    </div>
+                    
+                </div>
                 <div className='row p-2'>
                     {
                     vacations.map((vac, index) => 
@@ -208,22 +305,36 @@ function VacationsPage() {
                         return (
                         <div key={"vac" + index}  className="col-3 g-4">
                             <div className="card ">
-                                <h4> {vac.title} </h4>
                                 <div>
-                                    <div className='container '>
+                                    <div className=' '>
                                         <img className='col-12 vacimage' src={"http://localhost:3001/images/" + vac.image}/> 
+                                    </div>
+                                    <div className='rounded-top'>
+                                        <h4> {vac.title} </h4>
+                                        <div className='underTitle rounded-top'>
+                                            <div className=' col-12 date'>{vac.startDate.toDateString()} - {vac.endDate.toDateString()} </div>
+                                            <div className="underDate rounded-top">
+                                                <p> {vac.description} </p>
+                                                <div className='row d-flex justify-content-between px-1 mx-auto col-10'> 
+                                                    
+                                                    <div className='col-6'>
+                                                        <button type="button" className="btn btn-primary shadow mb-2 order-"> Order</button>
 
-                                    </div>
-                                    <p> {vac.description} </p>
-                                    <div className='row'> 
-                                        <p className="mt-0 col ">Price: ${vac.price}</p>
+                                                    </div>
+                                                    <div className='col-6 '>
+                                                        <p className="bold">Price: ${vac.price}</p>
 
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                        
+                                        
                                     </div>
-                                    <div className='d-flex row px-4 '>
-                                        <button type="button" className="btn btn-primary shadow mb-2 text-align-start col-3"> Order</button>
-                                    </div>
-                                    
                                 </div>
+                                
                             </div>
                         </div>
                     );
